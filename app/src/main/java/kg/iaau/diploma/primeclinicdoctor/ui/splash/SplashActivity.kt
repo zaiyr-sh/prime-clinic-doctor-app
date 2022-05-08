c6d164d72cbd0268410f7f2c88117ca7f16fdee3
Zaiyr <zaiyr.00@gmail.com>
2022-05-08 22:17:18 +0600

    Version app setting

--- app/src/main/java/kg/iaau/diploma/primeclinicdoctor/ui/splash/SplashActivity.kt
+++ app/src/main/java/kg/iaau/diploma/primeclinicdoctor/ui/splash/SplashActivity.kt
@@ -5,6 +5,8 @@ import android.os.Looper
 import android.view.LayoutInflater
 import dagger.hilt.android.AndroidEntryPoint
 import kg.iaau.diploma.core.ui.CoreActivity
+import kg.iaau.diploma.primeclinicdoctor.BuildConfig
+import kg.iaau.diploma.primeclinicdoctor.R
 import kg.iaau.diploma.primeclinicdoctor.databinding.ActivitySplashBinding
 import kg.iaau.diploma.primeclinicdoctor.ui.authorization.AuthorizationActivity
 import kg.iaau.diploma.primeclinicdoctor.ui.authorization.AuthorizationVM
@@ -17,6 +19,7 @@ class SplashActivity : CoreActivity<ActivitySplashBinding, AuthorizationVM>(Auth
         get() = ActivitySplashBinding::inflate
 
     override fun setupActivityView() {
+        vb.appVersion.text = getString(R.string.app_version, BuildConfig.VERSION_NAME)
         Handler(Looper.getMainLooper()).postDelayed({
             if (vm.isUserSignIn())
                 PinActivity.startActivity(this)
